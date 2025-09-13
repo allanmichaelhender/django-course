e6f7b3b47867e50fc12401b5332c33a19ef91fd1
Allan <allanhender@gmail.com>
2025-09-13 14:44:12 +0100

    adding user authorization

--- myproject/users/urls.py
+++ myproject/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name = 'posts'
+app_name = 'users'
 
 urlpatterns = [
-    path('', views.posts_list, name="list"),
-    path('<slug:slug>', views.post_page, name="page"),
+    path('register/', views.register_view, name="register"),
+    path('login/', views.login_view, name="login"),
+    path('logout/', views.logout_view, name="logout"),
 ]
