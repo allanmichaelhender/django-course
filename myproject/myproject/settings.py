6d7fa34677a2724683a07085694b1b363ea20370
Allan <allanhender@gmail.com>
2025-09-13 13:53:33 +0100

    adding images and starting new user app

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'posts',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -120,6 +121,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
